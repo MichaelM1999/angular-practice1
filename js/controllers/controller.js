9f4eef5e8738dcea10541475a042c0813657b0e0
michael mariea <mariea.michael@gmail.com>
2019-11-23 17:57:26 -0700

    more buttons

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -1,9 +1,22 @@
 app.controller("MainController", ['$scope', function ($scope) {
+    // to-do logic
     $scope.list = ["Clean", "go to the store", "study angular"];
     $scope.addItem = function() {
         $scope.list.push($scope.addToDo);
     }
-    $scope.bills = [];
+    // bills logic
+    $scope.bills = [{
+        name:"food",
+        ammount:"$20"
+    },
+    {
+        name:"car payment",
+        ammount:"$280"
+    },
+    {
+        name:"insurance",
+        ammount:"$200"
+    }];
     $scope.addBill = function() {
         $scope.bills.push({
             name: $scope.billName,
